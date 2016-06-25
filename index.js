c92478a591c202e110e01a17d9e0502783e53ffc
Kuriyama Hibiya <kuriyama@hibiya.moe>
2016-06-26 03:47:43 +0900

    complete rewrite; it works

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
 'use strict'
 
+const progress = require('progress')
 //const filters = require('./lib/require-all')('filters')
 
 const Crawler = require('./lib/Crawler')
 
 
+/*let prog = new progress(this.progress.format, {
+  complete: '=',
+  incomplete: ' ',
+  width: 30,
+  total: this.progress.width
+})*/
+
 let c = new Crawler('rhythmgame', {
   type: 'page',
-  until: 50,
-  progress: true
+  until: 50
 })
 
-c.crawl(console.log)
+c.on('start', function(latestId) {
+  console.log(`firstpage id: ${latestId}`)
+})
+
+c.on('progress', function(page, id) {
+  console.log(`${page}p, #${id}`)
+})
+
+c.on('complete', function(pages) {
+  console.log(pages.length)
+})
+
+c.start()
