f31701d99753733bd48e7d66eab1e624654c36ed
Kuriyama Hibiya <kuriyama@hibiya.moe>
2016-06-27 23:06:37 +0900

    Event-driven!

--- index.js
+++ index.js
@@ -1,35 +1,73 @@
 'use strict'
 
 const progress = require('progress')
-//const filters = require('./lib/require-all')('filters')
+const async = require('async')
+const fs = require('fs')
 
+const filters = require('./lib/require-all')('../filters')
 const Crawler = require('./lib/Crawler')
 
-let prog
-let c = new Crawler('rhythmgame', {
-  type: 'page',
-  until: 10
+const gall = 'rhythmgame'
+
+let prog, message = ''
+let c = new Crawler(gall, {
+  type: 'id',
+  until: 7938081
 })
 
 c.on('start', function(latestId, total) {
-  console.log(`total page: ${total}`)
+  console.log(`total page: ${total}p`)
 
-  prog = new progress(':title :current/:total? [:bar] :percent, :elapseds, eta :eta, #:cid', {
+  prog = new progress(':title :current/:total [:bar] :percent, :elapseds, eta :etas', {
    complete: '=',
    incomplete: ' ',
    width: 30,
    total: total
- })
+  })
+  prog.tick(0, {
+    title: gall,
+    message: message
+  })
 })
 
-c.on('progress', function(page, id) {
+c.on('progress', function(page, id, drops) {
   prog.tick(1, {
+    title: gall,
     cid: id
   })
 })
 
+c.on('dropped', function(page, drops) {
+  message = `[!] ${drops} dropped!`
+})
+
+c.on('error', function(e) {
+  console.error(e)
+})
+
 c.on('complete', function(pages) {
-  console.log(pages)
+  prog.terminate()
+  prog = new progress('collecting :current/:total [:bar] :percent, :elapseds, eta :etas', {
+    complete: '=',
+    incomplete: ' ',
+    width: 30,
+    total: Object.keys(pages).length
+  })
+
+  for(let id in pages) {
+    let article = pages[id]
+    for(let f in filters) {
+      filters[f].append(article)
+    }
+    prog.tick()
+  }
+
+  let result = {}
+  for(let f in filters) {
+    result[f] = filters[f].pop()
+  }
+  fs.writeFileSync('./result.json', JSON.stringify(result, null, 2))
+
 })
 
 c.start()
